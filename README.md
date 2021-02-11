46df59f877c3aec24c05d051c507760dd5d09af1
vilmar-moreira-souza <vilmarmoreira2010@gmail.com>
2021-02-11 03:15:32 -0300

    validação apenas numeros pelo teclado

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 <img src="./img/calculadora_1.png" width="340" alt="print da tela">
 
 <h3>sobre a aplicação</h3>
-<p>essa aplicação é uma calculadora simoples, onde é possivel fazer calculos de adição, subtração, divisão e multiplicação.<br>
+<p>essa aplicação é uma calculadora simples, onde é possivel fazer calculos de adição, subtração, divisão e multiplicação.<br>
 Pode ser utilizada pelo teclado ou pelo mouse.   </p>
 
 <h3>ideias para implementar</h3>
@@ -17,6 +17,6 @@ Pode ser utilizada pelo teclado ou pelo mouse.   </p>
 <li> mais...</li>
 
 <h3>bugs e problemas</h3>
-<li>E possível colocar letras na calculadora pelo teclado</li>
-<li> e possível colocar sinais iguais consecutivos, por exemplo (++++++)</li>
+
+<li> é possível colocar sinais iguais consecutivos, por exemplo (++++++)</li>
 
--- js/app.js
+++ js/app.js
@@ -1,12 +1,36 @@
 "use stritc"
-// adiçao: impedir letras
-
 
 let teclado = document.querySelector('#teclado')
 
-teclado.addEventListener("keyup", (event)=>{   
-  if(event.key === "Enter"){    
-    teclado.value= eval(teclado.value)
+teclado.addEventListener("keypress", (event) => {
+  //console.log(event)  ver teclado 
+  if (event.key === "Enter") {
+    teclado.value = eval(teclado.value)
   }
 })
+
+
+function validar(event) {
+  if (event.key == "0" || event.key == "Enter" || event.key == "1" || event.key == "4" ||
+    event.key == "2" || event.key == "5" ||
+    event.key == "3" || event.key == "6" ||
+    event.key == "7" || event.key == "9" ||
+    event.key == "8" || event.key == "/" ||
+    event.key == "*" || event.key == "-" ||
+    event.key == "+" ||
+    event.key == "."
+
+  ) { } else
+    if (event.key == ",") {// troca , por .
+      event.preventDefault()
+      teclado.value += "."
+    }
+    else {
+      event.preventDefault()//impedi de mostrar letra, so aceiat numero
+      //console.log(event.charCode)  ver teclado
+
+    }
+}
+
+
 
